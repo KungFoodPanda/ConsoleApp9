f27da3daeb52b07f5c62316e70cde2d299651d42
ConsoleApp9 <fiko1995@abv.bg>
2024-03-11 18:03:52 +0200

    Project upload

---
+++ student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+	public class Student
+	{
+		private string firstname;
+		private string lastname;
+		private int age;
+
+		public Student(string firstnamne, string lastname, int age)
+		{
+			this.firstname = firstnamne;
+			this.lastname = lastname;
+			this.age = age;
+		}
+		public string Getfullname()
+		{
+			return $"{firstname}{lastname}";
+
+		}
+		public bool Isadult()
+		{
+			return age >= 18;
+		}
+
+	}
+}
